a29e01b5315111e4a3b7b7b758531a3909c60fde
wenyan0303 <38025903+wenyan0303@users.noreply.github.com>
2018-05-24 10:59:45 +0800

    display page changes.
    Modify the overall layout of the page.

--- wx/task_content.php
+++ wx/task_content.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../inc/common.php';
-require_once('../db/staff_weixin.php');
+require_once '../db/staff_weixin.php';
 require_once '../db/staff_permit.php';
 
 // 需要员工登录
@@ -10,122 +10,59 @@ need_staff_login();
 <!DOCTYPE html>
 <html>
 <head>
-  <title>任务内容</title>
+  <title>任务详情</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
   <link rel="stylesheet" href="css/weui.css">
+  <link rel="stylesheet" href="css/example.css">
   <link rel="stylesheet" href="css/task_list.css">
 </head>
 <body>
-  <form class="page input js_show" action="../api/task_edit.php" method="GET" id="form" enctype="multipart/form-data">
+  <form class="page input js_show" method="GET" id="action" enctype="multipart/form-data">
     <div class="container">
       <div class="page input js_show">
         <!-- 任务详情 -->
-        <div class="weui-cells weui-cells_form content-item" style="margin-top: 0;">
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">项目</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" id="task_name" name="task_name" style="width: 100%" readonly="readonly">
-            </div>
+        <div class="weui-cell" style="display:none">
+          <div class="weui-cell__hd">
+            <label class="weui-label">任务ID</label>
           </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">监督人</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="check_name" id="check_name" readonly="readonly">
-            </div>
+          <div class="weui-cell__bd">
+            <input class="weui-input" name="task_id" id="task_id" readonly="readonly" value="<?php echo $_GET['task_id'] ?>">
           </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务等级</label>
+        </div>
+        <div class="page__hd" style="padding: 20px 20px 0 20px;">
+          <h1 class="page__title"><input type="text" readonly="readonly" id="task_status" id="task_status"><span id="task_name" class="task_name_c" name="task_name">蜂巢项目（hivebanks）中文版白皮书</span><input type="text" class="level" name="task_level" id="task_level" value="⭐⭐⭐" readonly="readonly"></h1>
+          <p class="page__desc" style="font-size: 14px;color: #000;">【监督人】<input type="text" name="check_name" class="check_name_c" id="check_name" value="袁浩" readonly="readonly">【价值】<input type="text" name="task_value" class="task_value_c" id="task_value" value="100" readonly="readonly"><br>【任务期限】<input type="text" name="limit_time" class="limit_time_c" id="limit_time" value="05-21" readonly="readonly">【创建日期】<input type="text" name="ctime" class="ctime_c" id="ctime" value="04-21" readonly="readonly"></p>
+          <div class="weui-slider-box">
+            <div class="weui-slider"   id="task_perc">
+              <div id="sliderInner" class="weui-slider__inner">
+                <div id="sliderTrack" style="width: 50%;" class="weui-slider__track"></div>
+                <div id="sliderHandler" style="left: 50%;" class="weui-slider__handler"></div>
+              </div>
             </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="task_level" id="task_level_l" readonly="readonly">
-            </div>
+            <div><span id="task_perc_l" name="task_perc" class="weui-slider-box__value">50</span></div>
+            <a class="weui-btn weui-btn_mini weui-btn_default" id="change">修改</a>
           </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务价值</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="task_value" id="task_value" readonly="readonly">
-            </div>
-          </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务进度</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="task_perc"  id="task_perc">
-            </div>
-          </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务状态</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="task_status" id="task_status" readonly="readonly">
-            </div>
-          </div>
-          <div class="weui-cell" style="display:none">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务ID</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="task_id" id="task_id" readonly="readonly" value="<?php echo $_GET['task_id'] ?>">
-            </div>
-          </div>
-          <div class="weui-cell" style="display:none">
-            <div class="weui-cell__hd">
-              <label class="weui-label">责任人</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-label" name="respo_name" id="respo_name" value="<?php echo $_GET['respo_name'] ?>">
-            </div>
-          </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务期限</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="limit_time" id="limit_time" readonly="readonly">
-            </div>
-          </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">创建时间</label>
-            </div>
-            <div class="weui-cell__bd">
-              <input class="weui-input" name="ctime" id="ctime" readonly="readonly">
-            </div>
-          </div>
-          <div class="weui-cell">
-            <div class="weui-cell__hd">
-              <label class="weui-label">任务内容</label>
-            </div>
-          </div>
+        </div>
+        <div class="page__bd">
           <div class="weui-cell content">
             <div class="weui-cell__bd">
-              <textarea class="weui-textarea" name="task_intro" style="width: 100%;" id="task_intro"></textarea>
+              <textarea class="weui-textarea" name="task_intro" id="task_intro" rows="20"></textarea>
             </div>
           </div>
           <div class="weui-btn-area">
-            <input class="weui-btn weui-btn_primary" type="submit" id="showTooltips" value="确定">
+            <a  class="weui-btn weui-btn_primary" id="showTooltips">确定</a>
           </div>
         </div>
       </div>
     </div>
   </form>
 
-  <script src="https://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
-  <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
-  <script src="js/swiper-4.2.2.min.js"></script>
+  <script src="js/zepto.min.js"></script>
   <script src="js/common.js"></script>
   <script src="js/task_info.js"></script>
   <script src="js/wx.js"></script>
-
+  <script src="https://res.wx.qq.com/open/js/jweixin-1.2.0.js"></script>
+ 
 </body>
 </html>
