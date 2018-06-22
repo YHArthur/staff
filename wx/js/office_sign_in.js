c8a8d2b596d7a97b34231d43daee06ae4451c4de
YHArthur <34911254+YHArthur@users.noreply.github.com>
2018-06-22 16:19:28 +0800

    0622

--- wx/js/office_sign_in.js
+++ wx/js/office_sign_in.js
@@ -4,52 +4,47 @@ window.onload = function(){
         window.location.href="http://www.fnying.com/staff/pc_forbiden.php?code=2";
     }
 }
-  
+
+window.shareData = {
+    // 分享标题
+    title: "员工微信签到",
+    // 分享描述
+    desc: "上海风赢网络科技有限公司员工签到页面【内部专用】",
+    // 分享链接
+    link: window.location.href,
+    // 分享图标
+    imgUrl: 'http://www.fnying.com/staff/wx/img/share.jpg',
+    success: function () {},
+    cancel: function () {}
+};
+
 $(function () {
-  // 分享标题
-  var ShareTitle = '员工微信签到';
-  // 分享描述
-  var ShareDesc = '上海风赢网络科技有限公司员工签到页面【内部专用】';
-  // 分享链接
-  var ShareLink = window.location.href;
-  // 分享图标
-  var ShareimgUrl = 'http://www.fnying.com/staff/wx/img/share.jpg';
-
-  // 微信配置启动
-  wx_config();
-
   // 展示员工签到记录
   staff_sign_log(10, 0);
-  
-  wx.ready(function() {
 
-      wx.onMenuShareTimeline({
-          title: ShareTitle,
-          desc: ShareDesc,
-          link: ShareLink,
-          imgUrl: ShareimgUrl
+  if (IsWeiXin()) {
+      $.getScript("https://res.wx.qq.com/open/js/jweixin-1.2.0.js", function () {
+          // 微信配置启动
+          wx_config();
+          wx.ready(function() {
+              wx.onMenuShareTimeline(shareData);
+              wx.onMenuShareAppMessage(shareData);
+              wx.getLocation({
+                  type: 'gcj02',
+                  success: function (res) {
+                      staff_sign(res.latitude, res.longitude);
+                    
+                  },
+                  cancel: function (res) {
+                      AlertDialog('地理位置获取失败，无法签到');
+                  },
+                  fail: function (res) {
+                      AlertDialog('地理位置获取失败，无法签到');
+                  }
+              });
+          });
       });
-
-      wx.onMenuShareAppMessage({
-          title: ShareTitle,
-          desc: ShareDesc,
-          link: ShareLink,
-          imgUrl: ShareimgUrl
-      });
-      
-      wx.getLocation({
-          type: 'gcj02',
-          success: function (res) {
-              staff_sign(res.latitude, res.longitude);
-            
-          },
-          cancel: function (res) {
-              AlertDialog('地理位置获取失败，无法签到');
-          }
-      });
-  });
-
-
+  }
 });
 
 // 员工签到处理
@@ -72,13 +67,13 @@ function staff_sign_log(limit, offset) {
     var post_data = {"limit": limit, "offset": offset};
     CallApi(api_url, post_data, function (response) {
         var log_id, staff_name, sign_type, ctime;
-        var clist = response.rows;
-        if (clist.length > 0) {
-            clist.forEach(function(value, index, array) {
-                log_id = value.log_id;
-                staff_name = value.staff_name;
-                sign_type = value.sign_type.replace('白金湾339', '');
-                ctime = value.ctime;
+        var rows = response.rows;
+        if (rows.length > 0) {
+            rows.forEach(function(row, index, array) {
+                log_id = row.log_id;
+                staff_name = row.staff_name;
+                sign_type = row.sign_type.replace('白金湾339', '');
+                ctime = row.ctime;
 
                 sign_row = '\
                 <label class="weui-cell weui-check__label" for="x' + log_id + '">\
--- wx/js/office_sign_out.js
+++ wx/js/office_sign_out.js
@@ -1,54 +1,50 @@
 // 禁止在微信以外的浏览器里打开
 window.onload = function(){
-  if(!IsWeiXin()){
-    window.location.href="http://www.fnying.com/staff/pc_forbiden.php?code=2";
-  }
+    if(!IsWeiXin()){
+        window.location.href="http://www.fnying.com/staff/pc_forbiden.php?code=2";
+    }
 }
-  
+
+window.shareData = {
+    // 分享标题
+    title: "员工微信签出",
+    // 分享描述
+    desc: "上海风赢网络科技有限公司员工签出页面【内部专用】",
+    // 分享链接
+    link: window.location.href,
+    // 分享图标
+    imgUrl: 'http://www.fnying.com/staff/wx/img/share.jpg',
+    success: function () {},
+    cancel: function () {}
+};
+
 $(function () {
-  // 分享标题
-  var ShareTitle = '员工微信签出';
-  // 分享描述
-  var ShareDesc = '上海风赢网络科技有限公司员工签出页面【内部专用】';
-  // 分享链接
-  var ShareLink = window.location.href;
-  // 分享图标
-  var ShareimgUrl = 'http://www.fnying.com/staff/wx/img/share.jpg';
-
-  // 微信配置启动
-  wx_config();
-
   // 展示员工签到记录
   staff_sign_log(10, 0);
 
-  wx.ready(function() {
-
-      wx.onMenuShareTimeline({
-          title: ShareTitle,
-          desc: ShareDesc,
-          link: ShareLink,
-          imgUrl: ShareimgUrl
+  if (IsWeiXin()) {
+      $.getScript("https://res.wx.qq.com/open/js/jweixin-1.2.0.js", function () {
+          // 微信配置启动
+          wx_config();
+          wx.ready(function() {
+              wx.onMenuShareTimeline(shareData);
+              wx.onMenuShareAppMessage(shareData);
+              wx.getLocation({
+                  type: 'gcj02',
+                  success: function (res) {
+                      staff_sign(res.latitude, res.longitude);
+                    
+                  },
+                  cancel: function (res) {
+                      AlertDialog('地理位置获取失败，无法签到');
+                  },
+                  fail: function (res) {
+                      AlertDialog('地理位置获取失败，无法签到');
+                  }
+              });
+          });
       });
-
-      wx.onMenuShareAppMessage({
-          title: ShareTitle,
-          desc: ShareDesc,
-          link: ShareLink,
-          imgUrl: ShareimgUrl
-      });
-      
-      wx.getLocation({
-          type: 'gcj02',
-          success: function (res) {
-              staff_sign(res.latitude, res.longitude);
-          },
-          cancel: function (res) {
-              AlertDialog('地理位置获取失败，无法签到');
-          }
-      });
-  });
-
-
+  }
 });
 
 // 员工签到处理
@@ -73,20 +69,20 @@ function staff_sign_log(limit, offset) {
         var log_id, staff_name, sign_type, ctime;
         var rows = response.rows;
         if (rows.length > 0) {
-          rows.forEach(function(row, index, array) {
-              log_id = row.log_id;
-              staff_name = row.staff_name;
-              sign_type = row.sign_type.replace('白金湾339', '');
-              ctime = row.ctime;
+            rows.forEach(function(row, index, array) {
+                log_id = row.log_id;
+                staff_name = row.staff_name;
+                sign_type = row.sign_type.replace('白金湾339', '');
+                ctime = row.ctime;
 
-              sign_row = '\
-              <label class="weui-cell weui-check__label" for="x' + log_id + '">\
-                <div class="weui-cell__bd">' + sign_type + ' <span>' + staff_name + '</span></div>\
-                <div class="weui-cell__ft">' + ctime.substr(5, 11) + '</div>\
-              </label>\
-              ';
-              $("#sign_rows").append(sign_row);
-          });
+                sign_row = '\
+                <label class="weui-cell weui-check__label" for="x' + log_id + '">\
+                  <div class="weui-cell__bd">' + sign_type + ' <span>' + staff_name + '</span></div>\
+                  <div class="weui-cell__ft">' + ctime.substr(5, 11) + '</div>\
+                </label>\
+                ';
+                $("#sign_rows").append(sign_row);
+            });
         }
     }, function (response) {
         AlertDialog(response.errmsg);
